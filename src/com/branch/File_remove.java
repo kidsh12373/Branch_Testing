f1a0889c7b666096077813f5efbea26e27793258
kishan <kishan@zap64.(none)>
2015-03-19 14:35:49 +0530

    try to delete

---
+++ src/com/branch/File_remove.java
@@ -0,0 +1,9 @@
+package com.branch;
+
+public class File_remove 
+{
+	public static void main(String args[])
+	{
+	System.out.println("This is the file for removing the through the terminal..");	
+	}
+}
