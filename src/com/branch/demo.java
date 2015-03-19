6310decd7c4ad13e7a9b78919217400f5e2ecf07
kishan <kishan@zap64.(none)>
2015-03-19 16:29:15 +0530

    Adding new demo file

---
+++ src/com/branch/demo.java
@@ -0,0 +1,9 @@
+package com.branch;
+
+public class demo 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Demo file");
+	}
+}
--- src/com/branch/file_remove.java
+++
@@ -1,9 +0,0 @@
-package com.branch;
-
-public class file_remove 
-{
-	public static void main(String args[])
-	{
-	System.out.println("File for testing....");	
-	}
-}
