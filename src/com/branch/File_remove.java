651619902c7affe85fe56a48e3216ca35f5aaeda
kishan <kishan@zap64.(none)>
2015-03-19 15:20:25 +0530

    I want's my file back...

--- src/com/branch/File_remove.java
+++
@@ -1,9 +0,0 @@
-package com.branch;
-
-public class File_remove 
-{
-	public static void main(String args[])
-	{
-	System.out.println("This is the file for removing the through the terminal..");	
-	}
-}
